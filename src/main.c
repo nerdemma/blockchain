a840b4926d4f13c7479586866bea4b7b631d7f3c
nerdemma <nerdemma@github.com>
2026-07-27 23:22:53 -0300

    output format

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 void test_stage1_hashing()
 {
-printf("------------- ETAPA 1: Prueba de Hashing SHA 256 ------------------");
+printf("------------- ETAPA 1: Prueba de Hashing SHA 256 ------------------\n");
 uint8_t out[32];
 char hex[65];
 sha256((const uint8_t*)"", 0, out);
@@ -18,12 +18,12 @@ int main()
 {
 test_stage1_hashing();
 uint32_t diff_bits = 16;
-printf("Creacion de Blockchain y Mineria. Dificultad: %u bits",diff_bits);
+printf("Creacion de Blockchain y Mineria. Dificultad: %u bits \n",diff_bits);
 Blockchain *chain = blockchain_create(diff_bits);
 char hex_hash[65];
 
 hex_encode(chain->blocks[0]->hash,32, hex_hash);
-printf("[Genesis] Hash: %s | nonce %u"
+printf("[Genesis] Hash: %s | nonce %u \n\n"
 , hex_hash, chain->blocks[0]->header.nonce);
 
 // minar bloque 1
@@ -32,18 +32,18 @@ mine_block(b1, diff_bits);
 blockchain_add_block(chain, b1);
 hex_encode(b1->hash, 32, hex_hash);
 
-printf("[Bloque 1] Hash: %s | nonce %u"
+printf("[Bloque 1] Hash: %s | nonce %u \n\n"
 , hex_hash, b1->header.nonce);
 
 //validar cadena
 printf("¿Cadena Valida?: %s",
-    blockchain_is_valid(chain) ? "SI (OK)" : "NO (CORRUPTA)");
+    blockchain_is_valid(chain) ? "SI (OK)\n" : "NO (CORRUPTA) \n");
 
     //simulacion de ataque (manipulacion de datos)
-    printf("Simulando alteracion maliciosa en el bloque 1");
+    printf("Simulando alteracion maliciosa en el bloque 1 \n");
     b1->data[0]='X';
     printf("¿cadena valida tras alteracion?: %s"
-    , blockchain_is_valid(chain) ? "SI (OK)" : "NO (DETECTADA)");
+    , blockchain_is_valid(chain) ? "SI (OK)\n" : "NO (DETECTADA) \n");
     blockchain_free(chain);
     return 0;
 }
